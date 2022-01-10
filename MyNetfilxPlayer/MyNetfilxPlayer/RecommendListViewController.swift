773bb0df0c32543c3dc21a529ab00cbf5e18d9bb
GoForWalk <atcgsp912@gmail.com>
2022-01-10 22:19:13 +0900

    2022 - 01 - 10
    SearchViewController
    	- URLSession
    	- Codable protocol & CodingKeys 로 data matching
    	- parsing Movie Data

--- MyNetfilxPlayer/MyNetfilxPlayer/RecommendListViewController.swift
+++ MyNetfilxPlayer/MyNetfilxPlayer/RecommendListViewController.swift
@@ -11,7 +11,7 @@ class RecommendListViewController: UIViewController {
     
     @IBOutlet weak var sectionTitle: UILabel!
     
-    let viewModel = RecommandListViewModel()
+    let viewModel = RecommendListViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +34,7 @@ extension RecommendListViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommendCell", for: indexPath) as? RecommandCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommendCell", for: indexPath) as? RecommendCell else {
             return UICollectionViewCell()
         }
         
@@ -50,8 +50,8 @@ extension RecommendListViewController: UICollectionViewDelegateFlowLayout {
     }
 }
 
-class RecommandListViewModel {
-    enum RecommandingType {
+class RecommendListViewModel {
+    enum RecommendingType {
         case award
         case hot
         case my
@@ -69,7 +69,7 @@ class RecommandListViewModel {
         }
     }
     
-    private (set) var type: RecommandingType = .my
+    private (set) var type: RecommendingType = .my
     private var items: [DummyItem] = []
     
     var numOfItems: Int {
@@ -80,7 +80,7 @@ class RecommandListViewModel {
         return items[index]
     }
     
-    func updateType(_ type: RecommandingType) {
+    func updateType(_ type: RecommendingType) {
         self.type = type
     }
     
@@ -89,7 +89,7 @@ class RecommandListViewModel {
     }
 }
 
-class RecommandCell: UICollectionViewCell {
+class RecommendCell: UICollectionViewCell {
     @IBOutlet weak var thumbnailImage: UIImageView!
     
     func updateUI(movie: DummyItem) {
@@ -98,26 +98,21 @@ class RecommandCell: UICollectionViewCell {
 }
 
 class MovieFetcher {
-    static func fetch(_ type: RecommandListViewModel.RecommandingType) -> [DummyItem] {
+    static func fetch(_ type: RecommendListViewModel.RecommendingType) -> [DummyItem] {
         switch type {
         case .award:
-            let movies = (1..<10).map{
-                DummyItem(thumbnail: UIImage(named: "img_movie_\($0)")!)
-            }
+            let movies = (1..<10).map { DummyItem(thumbnail: UIImage(named: "img_movie_\($0)")!) }
             return movies
         case .hot:
-            let movies = (10..<19).map{
-                DummyItem(thumbnail: UIImage(named: "img_movie_\($0)")!)
-            }
+            let movies = (10..<19).map { DummyItem(thumbnail: UIImage(named: "img_movie_\($0)")!) }
             return movies
         case .my:
-            let movies = (1..<10).map{ $0 * 2 }.map {
-                DummyItem(thumbnail: UIImage(named: "img_movie_\($0)")!)
-            }
+            let movies = (1..<10).map { $0 * 2 }.map { DummyItem(thumbnail: UIImage(named: "img_movie_\($0)")!) }
             return movies
         }
     }
 }
+
 
 struct DummyItem {
     let thumbnail: UIImage
--- MyNetfilxPlayer/MyNetfilxPlayer/SearchViewController.swift
+++ MyNetfilxPlayer/MyNetfilxPlayer/SearchViewController.swift
@@ -9,9 +9,146 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+//  viewController 누르면 키보드 내려가게 구현
+    @IBAction func tapBG (_ sender: Any) {
+        searchBar.resignFirstResponder()
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    // keyborad에서 Search button을 눌렀을 때, 검색하는 기능 구현
+    
+    private func dismissKeyboard() {
+        // searcnBar를 누르면 FirstResponder로 올라간다.
+        // 검색 누르면 키보드 내려가게 구현
+        searchBar.resignFirstResponder()
+    }
+    
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // search 버튼 누르는 순간
+        
+        // 검색 시작
+        
+        // 키보드 올라와 있을 때, 내려가게 처리
+        dismissKeyboard()
+        // 검색어가 있는지
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else {return}
+
+        // 네트워킹을 통한 검색
+        // - 목표: 서치텀을 가지고 네트워킹을 통해서 영화 검색
+        // - 검색 API가 필요
+        // - 결과를 받아올 모델 Movie, Response
+        // - 결과를 받아와서 CollectionView로 표현하기
+        
+        // instance method
+        
+        // type method
+        SearchAPI.search(searchTerm) {movies in
+            // collectionView로 표현하기
+            print("---> SearchAPI.search / resultCount: \(movies.count), First Movie Title: \(movies.first?.title)")
+        }
+        
+        print("---> 검색어: \(searchTerm)")
+    }
+}
+
+class SearchAPI {
+    // type method vs instance method
+    static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
+        // What is @escaping? : 해당 param의 코드 블럭이 현재 method 밖에서 실행된다.
+        // swift document 참고
+        
+        // URLSession 사용
+        let session = URLSession(configuration: .default)
+        
+        var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+        let mediaQuery = URLQueryItem(name: "media", value: "movie")
+        let entityQuery = URLQueryItem(name: "entity", value: "movie")
+        let itemQuerty = URLQueryItem(name: "term", value: term)
+        
+        urlComponents.queryItems?.append(itemQuerty)
+        urlComponents.queryItems?.append(entityQuery)
+        urlComponents.queryItems?.append(mediaQuery)
+        let requestURL = urlComponents.url!
+        
+        let dataTask = session.dataTask(with: requestURL) {data, response, error in
+            // TODO: Data 받아와서 Parsing
+            let successRange = 200..<300
+            guard error == nil,
+                  let statusCode = (response as? HTTPURLResponse)?.statusCode,
+                    successRange.contains(statusCode) else {
+                        completion([]) // @escaping
+                        print("---> NetWork Error\(String(describing: error?.localizedDescription))")
+                        return
+                    }
+            
+            guard let resultData = data else {
+                completion([])
+                return
+            }
+            
+            // data -> [Movie] : Parsing 필요
+//            let string = String(data: resultData, encoding: .utf8)
+            
+            // object parsing
+            let movies = SearchAPI.parseMovies(resultData)
+            completion(movies) // @escaping 으로 completion 넘겨준다.
+            print("---> result: \(movies.count)")
+            
+        } // end completionHandler closure
+        
+        //dataTask 실행
+        dataTask.resume()
+        
+    } // end func Search
+    
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
+        
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+        } catch let error{
+            print(" ---> Parsing error: \(error.localizedDescription)")
+            return []
+        }
+    } // end func parseMovies
+} // end Class SearchAPI
+
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
+}
+
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
+    
 }
